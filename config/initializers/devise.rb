ce2e4fd7950847688f04c4ab926a6a2f94ff156b
Tomas Herrera <tomasherrera91@hotmail.com>
2012-12-11 18:07:40 -0500

    Change Facebook APP details

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -16,7 +16,7 @@ Devise.setup do |config|
   # available as additional gems.
   require 'devise/orm/active_record'
   require "omniauth-facebook"
-    config.omniauth :facebook, "261205964006694", "5c3de93fe68c7a150df024a64431432c", {:scope => 'user_birthday'}
+    config.omniauth :facebook, "379325155490483", "8308f6068034f5f1d4111f1055e79410", {:scope => 'user_birthday'}
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
