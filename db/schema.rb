107e0f625d232b01e050909d76c823a3c31257b2
Hernan Orozco <horozco15@gmail.com>
2012-12-11 17:24:19 -0500

    add db schema to gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121211211434) do
-
+ActiveRecord::Schema.define(:version => 20121211214536) do
 
   create_table "categories", :force => true do |t|
     t.string   "name_category"
