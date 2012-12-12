1dadf2f429cdcf7175eebd90ea803a842302994f
Tomas Herrera <tomasherrera91@hotmail.com>
2012-12-12 16:51:00 -0500

    Corrected Hernan's Controller

--- app/controllers/complete_task_controller.rb
+++ app/controllers/complete_task_controller.rb
@@ -1,8 +1,8 @@
 class CompleteTaskController < ApplicationController
- def change_status
+  def change_status
     @task = Task.find(params[:id])
     (@task.status == true) ? @task.status = false : @task.status = true
     @task.save
-      format.html {redirect_to @list
+    redirect_to @task.list
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Todo2::Application.routes.draw do
  
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  
+  match "lists/:list_id/tasks/:id/change_status" => "complete_task#change_status", :as => :complete_task
   root :to => "home#index"
   resources :lists do
       resources :tasks
