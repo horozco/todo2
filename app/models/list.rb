b257f3660661d9b2c35b8b6ee1085f0761ec3eb6
Hernan Orozco <horozco15@gmail.com>
2012-12-13 18:11:53 -0500

    add validation for list model.

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ActiveRecord::Base
   attr_accessible :title, :description, :user_id, :category_id
-  validates_presence_of :title
+  validates_presence_of :title, :category_id
 
   has_many :tasks, dependent: :destroy
 
