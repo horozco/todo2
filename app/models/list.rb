db8eea1309cb55461c3ab9b9da0dbff5d0100509
Hernan Orozco <horozco15@gmail.com>
2012-12-13 14:50:29 -0500

    resolve conflicts

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,11 +1,11 @@
 class List < ActiveRecord::Base
   attr_accessible :title, :description, :user_id, :category_id
   validates_presence_of :title
-<<<<<<< HEAD
+
   has_many :tasks, dependent: :destroy
-=======
+
   has_many :tasks
   has_one :category
->>>>>>> Change Categories
+
   belongs_to :user
 end
